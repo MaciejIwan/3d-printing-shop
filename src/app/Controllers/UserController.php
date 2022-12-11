7fa64b5363e80508797c26797df430e5aa85bc47
MaciejIwan <64867638+MaciejIwan@users.noreply.github.com>
2022-12-11 19:33:39 +0100

    add register endpoint

--- src/app/Controllers/UserController.php
+++ src/app/Controllers/UserController.php
@@ -8,9 +8,16 @@ use App\Attributes\Put;
 use App\Attributes\Route;
 use App\Enums\HttpMethod;
 use App\View;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
 
 class UserController
 {
+    public function __construct(protected MailerInterface $mailer){
+
+    }
     #[Get('/login')]
     #[Route('/login2', HttpMethod::Head)]
     public function login(): View
@@ -29,4 +36,44 @@ class UserController
     {
 
     }
+
+    #[Get('/users/create')]
+    public function create(): View
+    {
+        return View::make('register');
+    }
+
+    #[Post('/users')]
+    public function register()
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $firstName = explode(' ', $name)[0];
+// todo move HTML to VIEW. Use Twing and check email templetes
+        $text = <<<Body
+Hello $firstName,
+Thank you for signing up!
+Body;
+
+        $html = <<<HTMLBody
+<h1 style="text-align: center; color: blue;">Welcome</h1>
+Hello $firstName,
+<br /><br />
+Thank you for signing up!
+HTMLBody;
+
+        $email = (new Email())
+            ->from('noreplay@3dprintingapp.com')
+            ->to($email)
+            ->subject('Welcome!')
+            ->attach('Hello World!', 'welcome.txt')
+            ->text($text)
+            ->html($html);
+
+//        $transport = Transport::fromDsn($_ENV['MAILER_DSN']);
+//        $mailer = new Mailer($transport);
+//        $mailer->send($email);
+        $this->mailer->send($email);
+    }
+
 }
