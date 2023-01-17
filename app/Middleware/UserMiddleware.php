0583b0f56c382cd62b4a3542e111d8b812aed1ec
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-01-17 03:31:02 +0100

    add UserMiddleware.php

---
+++ app/Middleware/UserMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use App\Contracts\AuthInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Views\Twig;
+
+class UserMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly AuthInterface            $auth,
+        private readonly Twig                     $twig
+    )
+    {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if ($user = $this->auth->user()) {
+            $this->twig->getEnvironment()->addGlobal('auth', ['id' => $user->getId(), 'name' => $user->getName()]);
+        }
+        return $handler->handle($request);
+    }
+}
