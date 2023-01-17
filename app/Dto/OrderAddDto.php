81a4efa3ff01281157007ca31dd3a268f988cd24
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-01-17 04:49:03 +0100

    update

--- app/Dto/OrderAddDto.php
+++ app/Dto/OrderAddDto.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
+use App\Entity\User;
 use App\Enum\OrderStatus;
 
 class OrderAddDto
 {
 
     public function __construct(
+        public readonly string      $name,
         public readonly int         $amount,
-        public readonly OrderStatus $status
+        public readonly OrderStatus $status,
+        public readonly User        $user,
     )
     {
+    }
+
+    public static function fromArrayAndUser(array $data, User $user): OrderAddDto
+    {
+        return new static(
+            $data['name'],
+            intval($data['amount']),
+            OrderStatus::New,
+            $user,
+        );
     }
 }
